7f090062c6cacc5c9b6b83cb29e1ffd0d09773ac
adibrisan <adi.brisan@yahoo.com>
2023-02-16 16:30:06 +0200

    API testing

---
+++ tests/api.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+test.describe.parallel("API Testing", () => {
+  const BASE_URL = "https://reqres.in/api";
+
+  test("Assert response status", async ({ request }) => {
+    const response = await request.get(`${BASE_URL}/users/2`);
+
+    expect(response.status()).toBe(200);
+  });
+
+  test("Expect list of users not to be null", async ({ request }) => {
+    const response = await request.get(`${BASE_URL}/users?page=2`);
+
+    const resBody = JSON.parse(await response.text());
+
+    expect(resBody).not.toBeNull();
+  });
+});
