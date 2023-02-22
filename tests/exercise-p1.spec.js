ca06a92eb8db64bee38fce0ea6db1625e39f9b20
adibrisan <adi.brisan@yahoo.com>
2023-02-22 17:23:55 +0200

    Added solution here

--- tests/exercise-p1.spec.js
+++ tests/exercise-p1.spec.js
@@ -1,4 +1,28 @@
 import { test, expect } from "@playwright/test";
 
 // Write your solution here
-test.describe("Login test exercise", () => {});
+test.describe("Login test exercise", () => {
+  test("Bad login", async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/");
+
+    await page.locator('[data-test="username"]').click();
+    await page.locator('[data-test="username"]').fill("wrong_username");
+    await page.locator('[data-test="password"]').click();
+    await page.locator('[data-test="password"]').fill("wrong_password");
+    await page.locator('[data-test="login-button"]').click();
+
+    await expect(
+      page.locator(
+        "#login_button_container > div > form > div.error-message-container.error > h3"
+      )
+    ).toBeVisible();
+
+    await expect(
+      page.locator(
+        "#login_button_container > div > form > div.error-message-container.error > h3"
+      )
+    ).toHaveText(
+      "Epic sadface: Username and password do not match any user in this service"
+    );
+  });
+});
--- tests/exercise-p2.spec.js
+++ tests/exercise-p2.spec.js
@@ -1,4 +1,19 @@
 import { test, expect } from "@playwright/test";
 
 // Write your solution here
-test.describe("API test exercise", () => {});
+test.describe("API test exercise", () => {
+  const BASE_URL = "https://reqres.in/api";
+  test("Assert response status", async ({ request }) => {
+    const data = {
+      email: "eve.holt@reqres.in",
+      password: "cityslicka",
+    };
+
+    const response = await request.post(`${BASE_URL}/login`, {
+      data,
+      headers: { "Content-Type": "application/json" },
+    });
+
+    expect(response.status()).toBe(200);
+  });
+});
